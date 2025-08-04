ec7a3812b5dbfea9ebdfa1e8dc933f8892c3e20b
localcoin <localcoinapp@gmail.com>
2025-08-04 14:17:23 +0000

    main font should be open sans

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
+        body: ['"Open Sans"', 'sans-serif'],
         headline: ['"Bitter"', 'serif'],
         code: ['monospace'],
       },
