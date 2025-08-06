365af237f43b8a23d9ffa42232b0614bb44e6c6f
localcoin <localcoinapp@gmail.com>
2025-08-06 13:48:06 +0000

    i dont understand.. what needs to happen

--- src/components/auth/signup-form.tsx
+++ src/components/auth/signup-form.tsx
@@ -171,7 +171,7 @@ export function SignupForm() {
       </CardHeader>
       <CardContent>
         <div className="space-y-2">
-          <Button variant="outline" className="w-full" onClick={.handleGoogleSignIn}>
+          <Button variant="outline" className="w-full" onClick={handleGoogleSignIn}>
             <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 126 23.4 172.9 62.3l-68.6 68.6c-20.5-19.4-48-31.5-79.3-31.5-62.3 0-113.5 51.6-113.5 114.9s51.2 114.9 113.5 114.9c72.3 0 96.9-46.3 102.5-69.1H248v-85.3h236.1c2.3 12.7 3.9 26.9 3.9 41.4z"></path></svg>
             Sign up with Google
           </Button>
@@ -181,7 +181,7 @@ export function SignupForm() {
           </Button>
         </div>
         <div className="relative my-4">
-          <div className="absolute inset-0 flex items-.center">
+          <div className="absolute inset-0 flex items-center">
             <span className="w-full border-t" />
           </div>
           <div className="relative flex justify-center text-xs uppercase">
@@ -253,3 +253,5 @@ export function SignupForm() {
     </Card>
   )
 }
+
+    
