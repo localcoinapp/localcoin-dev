d86d245affa7f8e5c8e06512fa699aced00a1aeb
localcoin <localcoinapp@gmail.com>
2025-08-10 17:19:55 +0000

    Runtime Error
    Error: Cannot read properties of undefined (reading 'src

--- src/components/map-view.tsx
+++ src/components/map-view.tsx
@@ -23,9 +23,9 @@ const MapView = ({ merchants }: MapViewProps) => {
     // Manually set icon paths to prevent bundling issues
     delete (L.Icon.Default.prototype as any)._getIconUrl;
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default.src,
-      iconUrl: require('leaflet/dist/images/marker-icon.png').default.src,
-      shadowUrl: require('leaflet/dist/images/marker-shadow.png').default.src,
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+      iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
     });
     
     if (mapRef.current && !mapInstance.current) {
