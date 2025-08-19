449fb4038e113cfb15bfc1e4141a87b423cea907
localcoin <localcoinapp@gmail.com>
2025-08-19 11:50:21 +0000

    ok no there is this errror:
    use-websocket.js:42 WebSocket connection to

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,14 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ],
-      },
-    ];
+  webpack: (config, { dev, isServer }) => {
+    // Enable polling to fix HMR in some environments
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
   },
 };
 
