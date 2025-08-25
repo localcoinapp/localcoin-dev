1b418b5c6dd39de47ee761497084a153135f8c3d
localcoin <localcoinapp@gmail.com>
2025-08-25 16:07:58 +0000

    but i already had one: /** @type {import('next').NextConfig} */ const ne

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,16 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  webpack: (config, { dev, isServer }) => {
+    // Enable polling to fix HMR in some environments
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
