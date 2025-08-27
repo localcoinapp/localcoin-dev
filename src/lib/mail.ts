09a3c45424dcef6c667313b216b3b9f6fe9cf119
localcoin <localcoinapp@gmail.com>
2025-08-27 17:53:11 +0000

    so everyhting kind of seems to work.. just a bug in the wallet process f

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,3 +1,4 @@
+
 // This file is intentionally left blank.
 // Email sending functionality has been moved to the /api/send-email API route
 // to prevent server-side libraries from being bundled with client-side code.
