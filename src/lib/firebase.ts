de5d05abecffcdb57bb334950d9ee8929efc4f90
localcoin <localcoinapp@gmail.com>
2025-08-28 09:43:07 +0000

    but you already did make these changes so un do them:
    import { initiali

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getFirestore, getDoc } from 'firebase/firestore';
 // == PASTE YOUR NEW FIREBASE CONFIGURATION OBJECT FROM STEP 1 HERE ==
 // ==================================================================
 const firebaseConfig = {
-    "projectId": "YOUR_NEW_PROJECT_ID",
-    "appId": "YOUR_NEW_APP_ID",
-    "storageBucket": "YOUR_NEW_STORAGE_BUCKET",
-    "apiKey": "YOUR_NEW_API_KEY",
-    "authDomain": "YOUR_NEW_AUTH_DOMAIN",
-    "messagingSenderId": "YOUR_NEW_MESSAGING_SENDER_ID"
+    "projectId": "localcoin-marketplace",
+    "appId": "1:929108967513:web:ddc99e36b6338aefc4b148",
+    "storageBucket": "localcoin-marketplace.appspot.com",
+    "apiKey": "AIzaSyDuU4uvBrYnyI6f6wk4n4hSCltJuJIYitg",
+    "authDomain": "localcoin-marketplace.firebaseapp.com",
+    "messagingSenderId": "929108967513"
 };
 // ==================================================================
 
